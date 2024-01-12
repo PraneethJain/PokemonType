b5bf7397da1b79502eca4f9d4c51fed6a557cf49
PraneethJain <praneethjain005@gmail.com>
2024-01-12 11:23:05 +0530

    refactor: useRandomPokemon hook

--- src/components/Guesser.jsx
+++ src/components/Guesser.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
 import "./Guesser.css";
+import { useRandomPokemon } from "../hooks/useRandomPokemon";
 import { getRandomPokedexNum } from "../utils/validPokemon";
 
 const correctGuessSound = new Audio("/correct_guess.mp3");
@@ -36,26 +37,6 @@ const types = [
   "steel",
   "water",
 ];
-
-const useRandomPokemon = (pokedexNum) => {
-  const [pokemonData, setPokemonData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  fetch(`https://pokeapi.co/api/v2/pokemon/${pokedexNum}`)
-    .then((response) => {
-      if (response.status >= 400) {
-        throw new Error("server error");
-      } else {
-        return response.json();
-      }
-    })
-    .then((response) => setPokemonData(response))
-    .catch((error) => setError(error))
-    .finally(() => setLoading(false));
-
-  return { pokemonData, error, loading };
-};
 
 function Type({ idx, selected, setSelected }) {
   const toggleSelected = () => {
